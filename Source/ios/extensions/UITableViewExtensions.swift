ab4302312a87fc8a9659fbfc7a6c46f99d906b9b
Jawwad Ahmad <jawwad.f.ahmad@gmail.com>
2017-10-10 10:12:16 +0500

    Add a convenience extension on UITableView and UICollectionView that can be used to dequeue the cell when the identifier matches the class name

--- Source/ios/extensions/UITableViewExtensions.swift
+++ Source/ios/extensions/UITableViewExtensions.swift
@@ -26,3 +26,27 @@ public extension UITableView {
         return cellForRow(at: IndexPath(row: row, section: section))!
     }
 }
+
+public extension UITableView {
+
+    // This method can be used when the reuseIdentifier matches the name of the class
+    func dequeue<CellClass>(_ cellClass: CellClass.Type, for indexPath: IndexPath) -> CellClass {
+        let reuseIdentifier = "\(cellClass)"
+        guard let cell = dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as? CellClass else {
+            fatalError("Could not find a cell with reuseIdentifier: \(reuseIdentifier). Please ensure that the reuseidentifier for \(cellClass) is set to \(reuseIdentifier)")
+        }
+        return cell
+    }
+}
+
+public extension UICollectionView {
+
+    // This method can be used when the reuseIdentifier matches the name of the class
+    func dequeue<CellClass>(_ cellClass: CellClass.Type, for indexPath: IndexPath) -> CellClass {
+        let reuseIdentifier = "\(cellClass)"
+        guard let cell = dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? CellClass else {
+            fatalError("Could not find a cell with reuseIdentifier: \(reuseIdentifier). Please ensuer that the reuseidentifier for \(cellClass) is set to \(reuseIdentifier)")
+        }
+        return cell
+    }
+}
