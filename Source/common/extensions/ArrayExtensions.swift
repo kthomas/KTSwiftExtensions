cfcd52436e9784463424351f2565aa7d8c5a0245
Jawwad Ahmad <jawwad.f.ahmad@gmail.com>
2017-10-22 09:30:10 +0500

    Remove obsolete methods.
    Array now has a "first" method

--- Source/common/extensions/ArrayExtensions.swift
+++ Source/common/extensions/ArrayExtensions.swift
@@ -9,26 +9,6 @@
 import Foundation
 
 public extension Array {
-    func findFirst(_ conditionBlock: (Element) -> Bool) -> Element? {
-        for object in self {
-            if conditionBlock(object) {
-                return object
-            }
-        }
-        return nil
-    }
-
-    func indexOfObject<T: Equatable>(_ obj: T) -> Int? {
-        var idx = 0
-        for elem in self {
-            if obj == elem as! T {
-                return idx
-            }
-            idx += 1
-        }
-        return nil
-    }
-
     func toJSON() -> String {
         let jsonData = encodeJSON(self)
         return NSString(bytes: (jsonData as NSData).bytes, length: jsonData.count, encoding: String.Encoding.utf8.rawValue)! as String
