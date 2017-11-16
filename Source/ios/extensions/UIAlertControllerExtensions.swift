bcdec9e767125ff159a4ffb09c0459a1bef6585c
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2017-11-16 17:46:48 -0500

    Cleanup alert view controller key window in view will disappear

--- Source/ios/extensions/UIAlertControllerExtensions.swift
+++ Source/ios/extensions/UIAlertControllerExtensions.swift
@@ -23,10 +23,10 @@ public extension UIAlertController {
         alertWindow.rootViewController!.present(self, animated: animated)
     }
 
-    open override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
+    open override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
 
-        if let windowLevel = UIApplication.shared.windows.last?.windowLevel, windowLevel > 1.0 {
+        if let vc = UIApplication.shared.keyWindow!.rootViewController?.presentedViewController, vc == self {
             let alertWindow = UIApplication.shared.keyWindow!
             alertWindow.rootViewController = nil
             alertWindow.isHidden = true
